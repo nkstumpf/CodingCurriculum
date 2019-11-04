94fcb8a18093de021dcc7573dec3c8a90af098f7
Nick Stumpf <nick.k.stumpf@gmail.com>
2019-11-04 00:33:55 -0500

    Fix file paths

--- 01_Core/week04/ecommerce/assets/javascript/buttons.js
+++ 01_Core/week04/ecommerce/assets/javascript/buttons.js
@@ -6,13 +6,13 @@ const shopNowBtn = document.getElementById('shopNowBtn');
 const allProductsBtn = document.getElementById('allProductsBtn');
 
 contactBtn.addEventListener('click', function() {
-    window.location.href = "/contact.html";
+    window.location.href = "https://nkstumpf.github.io/CodingCurriculum/01_Core/week04/ecommerce/contact.html";
 })
 
 shopNowBtn.addEventListener('click', function() {
-    window.location.href = "/allProducts.html";
+    window.location.href = "https://nkstumpf.github.io/CodingCurriculum/01_Core/week04/ecommerce/allProducts.html";
 })
 
 allProductsBtn.addEventListener('click', function() {
-    window.location.href = "/allProducts.html";
+    window.location.href = "https://nkstumpf.github.io/CodingCurriculum/01_Core/week04/ecommerce/allProducts.html";
 })
